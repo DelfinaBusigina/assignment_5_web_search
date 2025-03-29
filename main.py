bf06b489c5ee5f9c7b76445d5e490de11018deff
Delfina <delfina.busigina@gmail.com>
2025-03-29 09:42:11 +0200

    Loaded json, transformed to df

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import pandas as pd
 import plotly as plt
+import json
 from terminal_coloring import prGreen
 
 prGreen(" ============== Python is working! ==============")
 
 json_data = 'Dataset/trending.json'
 
-df = pd.read_json(json_data[1])
-print(df.to_string())
+# df = pd.json_normalize(json_data)
+
+with open(json_data, 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+# print(data)
+
+df = pd.json_normalize(data['collector'])
+print(df)
+
+# print(df.to_string())
