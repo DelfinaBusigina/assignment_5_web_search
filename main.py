eb41200ee1a9f532832ea347f40433b269a5efe9
Delfina <delfina.busigina@gmail.com>
2025-03-29 09:35:15 +0200

    Practiced working with json and pd df

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import pandas as p
+import pandas as pd
 import plotly as plt
+from terminal_coloring import prGreen
 
-print("Python is working!")
+prGreen(" ============== Python is working! ==============")
+
+json_data = 'Dataset/trending.json'
+
+df = pd.read_json(json_data[1])
+print(df.to_string())
---
+++ terminal_coloring.py
@@ -0,0 +1,18 @@
+
+# Green color in terminal
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+
+# Yellow color for terminal
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+
+# Light purple
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
+
+# Red
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+
+# Cyan
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
+ 
+# Light Gray
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk))
